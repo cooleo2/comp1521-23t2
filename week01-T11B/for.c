c2e4cacd00fcce138c28efbf36633bbf02fed049
Leonard <leochiang2002@gmail.com>
2023-05-30 12:06:05 +1000

    T W1

---
+++ week01-T11B/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    // print every number from 1 to 10 inclusive with a while loop
+
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+   return 0;
+}
